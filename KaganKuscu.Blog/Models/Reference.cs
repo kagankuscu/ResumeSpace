584d1c94987303281e366093e545448de9c349f1
kagankuscu <kuscukagan@gmail.com>
2024-02-27 00:20:41 +0300

    More Models added

--- KaganKuscu.Blog/Models/Reference.cs
+++ KaganKuscu.Blog/Models/Reference.cs
@@ -5,15 +5,13 @@ using System.Threading.Tasks;
 
 namespace KaganKuscu.Blog.Models
 {
-    public class User : BaseModel
+    public class Reference : BaseModel
     {
         public string Name { get; set; }
-        public int Age { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
+        public string Role { get; set; }
         public string ImagePath { get; set; }
-        public string ResumePath { get; set; }
+        public int UserId { get; set; }
     }
 }
