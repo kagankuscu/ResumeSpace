c051bc8a8eee1a401a3290289ea3b76cd3884d30
kagankuscu <kuscukagan@gmail.com>
2024-02-28 22:09:24 +0300

    Referance added

--- KaganKuscu.Blog/Controllers/HomeController.cs
+++ KaganKuscu.Blog/Controllers/HomeController.cs
@@ -1,18 +1,9 @@
-using KaganKuscu.Blog.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace KaganKuscu.Blog.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
