b6010aeb6a3ad7be356e076d1a11b5a7e85cb8f5
kagankuscu <kuscukagan@gmail.com>
2024-03-05 16:14:11 +0200

    New Migration
    Person added Email field
    SocialMedia added IconCssClass
    Intereset Table added.

--- KaganKuscu.Model/Models/Interest.cs
+++ KaganKuscu.Model/Models/Interest.cs
@@ -1,15 +1,13 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace KaganKuscu.Model.Models
 {
-    public class SocialMedia : BaseModel
+    public class Interest : BaseModel
     {
-        public string Name { get; set; }
-        public string Url { get; set; }
-        public int PersonId { get; set; }
         public string IconCssClass { get; set; }
     }
 }
--- KaganKuscu.Model/Models/Person.cs
+++ KaganKuscu.Model/Models/Person.cs
@@ -12,16 +12,19 @@ namespace KaganKuscu.Model.Models
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public string Address { get; set; }
+        public string Email { get; set; }
         public string Phone { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
         public string ImagePath { get; set; }
         public string ResumePath { get; set; }
+        public string? Interest { get; set; }
 
         public virtual ICollection<SocialMedia> SocialMedias { get; set; } = new List<SocialMedia>();
         public virtual ICollection<WorkExperience> WorkExperiences { get; set; } = new List<WorkExperience>();
         public virtual ICollection<Education> Educations { get; set; } = new List<Education>();
         public virtual ICollection<Reference> References { get; set; } = new List<Reference>();
         public virtual ICollection<Skill> Skills { get; set; } = new List<Skill>();
+        public virtual ICollection<Interest> Interests { get; set; } = new List<Interest>();
     }
 }
