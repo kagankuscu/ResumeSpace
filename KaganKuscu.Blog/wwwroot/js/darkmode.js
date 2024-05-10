f3d1b2bd1f3ad2db8c96065efc947c682d596725
kagankuscu <kuscukagan@gmail.com>
2024-05-10 16:01:16 +0300

    Admin Layout change theme

--- KaganKuscu.Blog/wwwroot/js/darkmode.js
+++ KaganKuscu.Blog/wwwroot/js/darkmode.js
@@ -1,10 +1,12 @@
 const theme = window.matchMedia('(prefers-color-scheme: dark)');
 changeTheme(theme.matches);
+changeThemeAdmin(theme.matches);
 
 
 window.matchMedia('(prefers-color-scheme: dark)')
             .addEventListener('change',({ matches }) => {
                     changeTheme(matches);
+                    changeThemeAdmin(matches);
                 });
 
 function changeTheme(matches) {
@@ -12,5 +14,13 @@ function changeTheme(matches) {
         $("html").removeClass("theme-skin-light").addClass("theme-skin-dark");
     } else {
         $("html").removeClass("theme-skin-dark").addClass("theme-skin-light");
+    }
+}
+
+function changeThemeAdmin(matches) {
+    if (matches) {
+        $("body").attr("data-bs-theme", "dark");
+    } else {
+        $("body").attr("data-bs-theme", "light");
     }
 }
