83a7e3fde1df9538c8ddde2a6770a9aca9066f0d
kagankuscu <kuscukagan@gmail.com>
2024-05-11 00:15:29 +0300

    DashboardController
    Path are created and views added.

--- KaganKuscu.Blog/Areas/Admin/Controllers/DashboardController.cs
+++ KaganKuscu.Blog/Areas/Admin/Controllers/DashboardController.cs
@@ -5,15 +5,55 @@ using Microsoft.AspNetCore.Mvc;
 namespace KaganKuscu.Blog.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class DashboardController : Controller
     {
         
-        [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             var username = User.FindFirstValue(ClaimTypes.Name);
 
             return View(model: username);
+        }
+
+        public IActionResult People()
+        {
+            return View();
+        }
+
+        public IActionResult Files()
+        {
+            return View();
+        }
+
+        public IActionResult Educations()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            return View();
+        }
+
+        public IActionResult WorkExperiences()
+        {
+            return View();
+        }
+
+        public IActionResult References()
+        {
+            return View();
+        }
+
+        public IActionResult SocialMedias()
+        {
+            return View();
+        }
+
+        public IActionResult Quotes()
+        {
+            return View();
         }
     }
 }
