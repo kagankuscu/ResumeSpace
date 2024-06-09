fe6bfbbecd56b2b3de095a5e85035d1542e179e4
kagankuscu <kuscukagan@gmail.com>
2024-06-09 12:55:11 +0300

    SocialMedia Name field removed

--- KaganKuscu.DataAccess/Config/SocialMediaConfig.cs
+++ KaganKuscu.DataAccess/Config/SocialMediaConfig.cs
@@ -9,9 +9,9 @@ namespace KaganKuscu.DataAccess.Config
         public void Configure(EntityTypeBuilder<SocialMedia> builder)
         {
             builder.HasData(
-                new SocialMedia { Id = 1, AppUserId = Guid.Parse("D0C23476-68D0-4DA0-AAD4-3ADAE20702C0"), Name = "Github", Url = "https://github.com/kagankuscu", SocialMediaIconId = 1 },
-                new SocialMedia { Id = 2, AppUserId = Guid.Parse("D0C23476-68D0-4DA0-AAD4-3ADAE20702C0"), Name = "LinkedIn", Url = "https://www.linkedin.com/in/kagan-kuscu/", SocialMediaIconId = 2 },
-                new SocialMedia { Id = 3, AppUserId = Guid.Parse("D0C23476-68D0-4DA0-AAD4-3ADAE20702C0"), Name = "Instagram", Url = "https://www.instagram.com/kagan_kuscu/", SocialMediaIconId = 3 }
+                new SocialMedia { Id = 1, AppUserId = Guid.Parse("D0C23476-68D0-4DA0-AAD4-3ADAE20702C0"), Url = "https://github.com/kagankuscu", SocialMediaIconId = 1 },
+                new SocialMedia { Id = 2, AppUserId = Guid.Parse("D0C23476-68D0-4DA0-AAD4-3ADAE20702C0"), Url = "https://www.linkedin.com/in/kagan-kuscu/", SocialMediaIconId = 2 },
+                new SocialMedia { Id = 3, AppUserId = Guid.Parse("D0C23476-68D0-4DA0-AAD4-3ADAE20702C0"), Url = "https://www.instagram.com/kagan_kuscu/", SocialMediaIconId = 3 }
             );
         }
     }
