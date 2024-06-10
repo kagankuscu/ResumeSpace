d4d2397c445e8a210b8c837bb6cc11c5cf872441
kagankuscu <kuscukagan@gmail.com>
2024-06-10 22:42:20 +0300

    Quotes are added

---
+++ KaganKuscu.Model/Dtos/QuoteDto/QuoteForAddDto.cs
@@ -0,0 +1,7 @@
+﻿namespace KaganKuscu.Model.Dtos.QuoteDto;
+
+public record QuoteForAddDto
+{
+    public string Owner { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
---
+++ KaganKuscu.Model/Dtos/QuoteDto/QuoteForGetDto.cs
@@ -0,0 +1,8 @@
+﻿namespace KaganKuscu.Model.Dtos.QuoteDto;
+
+public record QuoteForGetDto
+{
+    public Guid Guid { get; set; }
+    public string Owner { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
