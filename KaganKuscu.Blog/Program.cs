ab9f59abb04f002fa8f76c1326966cd5204e6902
kagankuscu <kuscukagan@gmail.com>
2024-08-07 22:07:56 +0300

    Home Routing Changed

--- KaganKuscu.Blog/Program.cs
+++ KaganKuscu.Blog/Program.cs
@@ -55,7 +55,7 @@ namespace KaganKuscu.Blog
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{username?}");
 
             app.Run();
         }
