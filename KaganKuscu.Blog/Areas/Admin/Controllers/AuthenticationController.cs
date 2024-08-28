80b1dec68602ea4d97d812a25d5b7f499a62cf52
kagankuscu <kuscukagan@gmail.com>
2024-08-28 16:17:09 +0300

    Controller View and Ajax method added

--- KaganKuscu.Blog/Areas/Admin/Controllers/AuthenticationController.cs
+++ KaganKuscu.Blog/Areas/Admin/Controllers/AuthenticationController.cs
@@ -1,5 +1,7 @@
 using KaganKuscu.Business.Abstract;
+using KaganKuscu.EmailService.Concrete;
 using KaganKuscu.Model.Dtos;
+using KaganKuscu.Model.Dtos.UserDto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KaganKuscu.Blog.Areas.Admin.Controllers
@@ -22,6 +24,14 @@ namespace KaganKuscu.Blog.Areas.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Recover()
+        {
+            return View();
+        }
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
         public IActionResult AccessDenied()
         {
             return View();
@@ -39,6 +49,16 @@ namespace KaganKuscu.Blog.Areas.Admin.Controllers
             return Json(await _authService.Register(userDto));
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Recover([FromBody] UserForForgotPasswordDto forgotPassword)
+        {
+            return Json(await _authService.Recover(forgotPassword));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Reset([FromBody] UserForResetPasswordDto userDto)
+        {
+            return Json(await _authService.Reset(userDto));
+        }
         public async Task<IActionResult> Logout()
         {
             if (!await _authService.Logout())
