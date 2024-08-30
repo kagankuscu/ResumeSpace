600f868a65d4198b69c0e44bc16f2447dad53bcb
kagankuscu <kuscukagan@gmail.com>
2024-08-30 15:11:03 +0300

    Sending EmailForSendDto created

---
+++ KaganKuscu.Model/Dtos/EmailDto/EmailForSendDto.cs
@@ -0,0 +1,11 @@
+namespace KaganKuscu.Model.Dtos.EmailDto
+{
+    public class EmailForSendDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? Subject { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
