b14fac787fe21524af684a77078145fc8c21f80d
kagankuscu <kuscukagan@gmail.com>
2024-08-30 12:35:08 +0300

    Logo changed

--- KaganKuscu.Blog/Areas/Admin/Controllers/DashboardController.cs
+++ KaganKuscu.Blog/Areas/Admin/Controllers/DashboardController.cs
@@ -10,7 +10,7 @@ namespace KaganKuscu.Blog.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Resumes");
         }
 
         public IActionResult Resumes()
